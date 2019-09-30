e59f4c0fcd2ac15f6981b6853c2188c239c0b14f
Scott DeHaan <scottdehaan@gmail.com>
2019-09-30 14:20:03 -0500

    First commit

--- retrosheet-wldata-builder/Program.cs
+++ retrosheet-wldata-builder/Program.cs
@@ -11,7 +11,7 @@ namespace retrosheet_wldata_builder
     {
         static void Main(string[] args)
         {
-            var input = "//Users//inkandthunder//Downloads//GL2018.TXT";
+            var input = "data/GL2018.TXT";
             List<Team> teams = GetTeams("data/TEAMABR.TXT");
             //List<Team> teams = new List<Team>();
             if (File.Exists(input))
@@ -27,8 +27,7 @@ namespace retrosheet_wldata_builder
 
                         foreach (var record in records)
                         {
-                            Console.WriteLine(LeagueLongFormatter(record.HomeTeamLeague) + " " + LeagueLongFormatter(record.VisitingTeamLeague));
-
+                            Console.WriteLine();
                             //Console.WriteLine(DateFormatter(record.Date) + " " + TeamNameLongFormatter(record.HomeTeamName, teams) + " (" + record.HomeTeamScore + ") vs. " + TeamNameLongFormatter(record.VisitingTeamName, teams) + " (" + record.VisitingTeamScore+ ")");
                         }
                         Console.WriteLine("Completed");
@@ -48,7 +47,7 @@ namespace retrosheet_wldata_builder
         }
 
 
-        public static string DateFormatter(string date)
+        public static string FriendlyDateFormat(string date)
         {
             DateTime dt = DateTime.ParseExact(date, "yyyyMMdd", CultureInfo.InvariantCulture);
             return dt.ToString("MM/dd/yyyy");
@@ -58,20 +57,18 @@ namespace retrosheet_wldata_builder
         {
             if (File.Exists(data))
             {
-                using (var reader = new StreamReader(data))
-                using (var csv = new CsvReader(reader))
-                {
-                    csv.Configuration.HeaderValidated = null;
-                    csv.Configuration.RegisterClassMap<TeamDataMap>();
-                    var teams = csv.GetRecords<Team>();
+                using var reader = new StreamReader(data);
+                using var csv = new CsvReader(reader);
+                csv.Configuration.HeaderValidated = null;
+                csv.Configuration.RegisterClassMap<TeamDataMap>();
+                var teams = csv.GetRecords<Team>();
 
-                    List<Team> output = new List<Team>();
-                    foreach (var team in teams)
-                    {
-                        output.Add(team);
-                    }
-                    return output;
+                List<Team> output = new List<Team>();
+                foreach (var team in teams)
+                {
+                    output.Add(team);
                 }
+                return output;
             }
             else
             {
@@ -88,52 +85,73 @@ namespace retrosheet_wldata_builder
         public static string TeamNameLongFormatter(string team, List<Team> teamData)
         {
             return teamData.Where(x => x.TeamAbbreviation == team).Select(v => v.City + " " + v.Nickname).First();
-            //return "";
         }
 
         public static string LeagueLongFormatter(string league)
         {
-            string longLeague;
-            switch (league)
+            var longLeague = league switch
             {
-                case "NA":
-                    longLeague = "National Association";
-                    break;
-                case "NL":
-                    longLeague = "National League";
-                    break;
-                case "AA":
-                    longLeague = "American Association";
-                    break;
-                case "AL":
-                    longLeague = "American League";
-                    break;
-                default:
-                    longLeague = league;
-                    break;
-            }
-
+                "NA" => "National Association",
+                "NL" => "National League",
+                "AA" => "American Association",
+                "AL" => "American League",
+                _ => league,
+            };
             return longLeague;
         }
 
         public static string GameInformationFormatter(string game)
         {
-            return "";
+            var gameInformation = game switch
+            {
+                "0" => "Single Game",
+                "1" => "First game of a double (or triple) header",
+                "2" => "Second game of a double (or triple) header",
+                "3" => "Third game of a triple header",
+                "A" => "First game of a double-header involving 3 teams",
+                "B" => "Second game of a double-header involving 3 teams",
+                _ => "",
+            };
+            return gameInformation;
         }
 
         public static string ForfeitFormatter(string forfeit)
         {
-            return "";
+            var forfeitInformation = forfeit switch
+            {
+                "V" => "Forfeited to the Visiting Team",
+                "H" => "Forfeited to the Home Team",
+                "T" => "No Decision",
+                _ => "",
+            };
+            return forfeitInformation;
         }
 
         public static string ProtestFormatter(string protest)
         {
-            return "";
+            var protestInformation = protest switch
+            {
+                "P" => "Protested by an unidentified team",
+                "V" => "Disallowed protest made by visiting team",
+                "H" => "Disallowed protest made by home team",
+                "X" => "Upheld protest made by visiting team",
+                "Y" => "Uphead protest made by home team",
+                _ => "",
+            };
+            return protestInformation;
         }
 
         public static string AcquisitionFormatter(string acquisition)
         {
-            return "";
+            var acquisitionInformation = acquisition switch
+            {
+                "Y" => "Complete game information unavailable",
+                "N" => "No portion of the game available",
+                "D" => "Game information derived from box score and game story",
+                "P" => "Partial game information available, but may be missing complete innings",
+                _ => "",
+            };
+            return acquisitionInformation;
         }
 
         //public static LineScore LineScoreParser(string linescore)
